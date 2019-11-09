631abba39158555c07c26a2a4a28d04c48e59c3e
matth <16mwkg@queensu.ca>
2019-11-09 16:09:30 -0500

    mentors fixed

--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -5,75 +5,39 @@ import MentorCard from "./MentorCard";
 
 // Mentor headshots
 import plchldr from "../assets/img/logo/qhacksAppIcon.png";
-
+import pascalSiakam from "../assets/img/headshots/pascal_siakam.png";
+import pm from "../assets/img/headshots/pm.png";
+import rapsLogo from "../assets/img/companyLogos/rapsLogo.png";
 const QHACKS_COLORS = ["#00205b", "#fedb01", "#c81c2e"];
 
 const mentors = [
   {
-    name: "Mentor 1",
-    title: "X at Y",
-    headshot: plchldr,
-    companyName: "Company",
-    companyUrl: "https://www.qhacks.io/"
+    name: "Pascal Siakam",
+    title: "Raptors",
+    headshot: pascalSiakam,
+    companyName: "Company Site",
+    companyLogo:rapsLogo,
+    companyUrl: "https://www.nba.com/raptors/interstitial-2"
   },
   {
-    name: "Mentor 2",
-    title: "X at Y",
-    headshot: plchldr,
-    companyName: "Company",
-    companyUrl: "https://www.qhacks.io/"
-  },
-  {
-    name: "Mentor 3",
-    title: "X at Y",
-    headshot: plchldr,
-    companyName: "Company",
-    companyUrl: "https://www.qhacks.io/"
-  },
-  {
-    name: "Mentor 4",
-    title: "X at Y",
-    headshot: plchldr,
-    companyName: "Company",
-    companyUrl: "https://www.qhacks.io/"
-  },
-  {
-    name: "Mentor 5",
-    title: "X at Y",
-    headshot: plchldr,
-    companyName: "Company",
-    companyUrl: "https://www.qhacks.io/"
-  },
-  {
-    name: "Mentor 6",
-    title: "X at Y",
-    headshot: plchldr,
-    companyName: "Company",
-    companyUrl: "https://www.qhacks.io/"
-  },
-  {
-    name: "Mentor 7",
-    title: "X at Y",
-    headshot: plchldr,
-    companyName: "Company",
-    companyUrl: "https://www.qhacks.io/"
-  },
-  {
-    name: "Mentor 8",
-    title: "X at Y",
-    headshot: plchldr,
-    companyName: "Company",
-    companyUrl: "https://www.qhacks.io/"
-  },
+    name: "Patrick Mccaw",
+    title: "Raptors",
+    headshot: pm,
+    companyName: "Company Site",
+    companyLogo:rapsLogo,
+    companyUrl: "https://www.nba.com/raptors/interstitial-2"
+  }
+
 ];
 
 export default () => (
   <div css={{ marginBottom: "16px" }}>
-    <h1 css={{ color: "#ffffff", fontWeight: "600", fontSize: "28px" }}>
+    <h1 css={{ color: "#ffffff", fontWeight: "600", fontSize: "28px" ,textAlign: "center",position: "relative",}}>
       Additional Mentors
     </h1>
     <div
       css={{
+        color: "#ffffff",
         display: "grid",
         "@media (min-width: 1284px)": {
           gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))"
@@ -89,8 +53,9 @@ export default () => (
       {mentors.map((mentor, i) => (
         <MentorCard
           {...mentor}
-          key={mentor.name}
+           key={mentor.name}
           borderColor={QHACKS_COLORS[i % 3]}
+          
         />
       ))}
     </div>
