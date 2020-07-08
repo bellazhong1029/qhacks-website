8502fca93266179e264528d5e4b36e013e224d7b
Carter Conboy <conboy2001@gmail.com>
2020-07-08 15:04:26 -0400

    Footer graphic design implementation

--- src/components/FooterGraphic.js
+++ src/components/FooterGraphic.js
@@ -1,7 +1,43 @@
 import React from "react";
 
+import ContentWrapper from "./ContentWrapper";
+
+import background from "../assets/img/backgrounds/FooterGraphic.png";
+import landingImg from "../assets/img/icons/landingImg.svg";
+
 const FooterGraphic = () => (
-  <div css={{ fontSize: "80px", textAlign: "center" }}>Footer graphic</div>
+  <footerGraphic css={{ zIndex: "9999" }}>
+    <div
+      id=""
+      css={{
+        width: "100%",
+        background: `url(${background}) no-repeat center center`,
+        backgroundSize: "cover",
+        "@media(max-width: 600px)": {
+          minHeight: "600px"
+        }
+      }}
+    >
+      <ContentWrapper>
+        <img
+          src={landingImg}
+          css={{
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto",
+            paddingBottom: "83px",
+            width: "75vw",
+            maxWidth: "350px"
+          }}
+          alt="QHacks Illustration"
+        />
+      </ContentWrapper>
+    </div>
+  </footerGraphic>
 );
 
 export default FooterGraphic;
+
+
+
+    
