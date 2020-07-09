fce0a5129eb9d241dfd68939b43d9fd426d042ab
Carter Conboy <conboy2001@gmail.com>
2020-07-09 12:41:49 -0400

    updated footer graphic so that circuits line up

--- src/components/FooterGraphic.js
+++ src/components/FooterGraphic.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import ContentWrapper from "./ContentWrapper";
 
-import background from "../assets/img/backgrounds/FooterGraphic.png";
+import background from "../assets/img/backgrounds/extendedGrayCircuits.png";
 import landingImg from "../assets/img/icons/landingImg.svg";
 import circuitGrey from "../assets/img/backgrounds/circuitsGrey.png";
 
@@ -13,7 +13,7 @@ const FooterGraphic = () => (
         position: "relative",
         width: "100%",
         height: "500px",
-        background: `url(${circuitGrey}) repeat-x center bottom`,
+        background: `url(${background}) repeat-x center bottom`,
         backgroundSize: "auto 300px",
         "@media(max-width: 820px)": {
           height: "300px",
